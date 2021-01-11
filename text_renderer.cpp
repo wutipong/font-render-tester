792ddec5ca1c5a01f6923077ebd1a9f48165bd35
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2021-01-12 02:34:00 +0700

    add some skeleton code for vertical text.

--- text_renderer.cpp
+++ text_renderer.cpp
@@ -91,3 +91,73 @@ void TextRenderLeftToRight(SDL_Renderer* renderer, const SDL_Rect& bound, Font& 
         y += -font.Descend() + font.LineGap() + font.Ascend();
     }
 }
+
+void TextRenderTopToBottom(SDL_Renderer* renderer, const SDL_Rect& bound, Font& font,
+    const std::string& str, const SDL_Color& color,
+    const hb_script_t& script) {
+
+    if (!font.IsValid())
+        return;
+
+    hb_font_extents_t extents;
+    hb_font_get_extents_for_direction(font.HbFont(), HB_DIRECTION_TTB, &extents);
+
+    float ascend = roundf(extents.ascender * font.Scale());
+    float descend = roundf(extents.descender * font.Scale());
+    float linegap = roundf(extents.descender * font.Scale());
+
+    auto u16str = utf8::utf8to16(str);
+    auto lineStart = u16str.begin();
+    int x = bound.w - ascend;
+
+
+    while (true) {
+
+        SDL_SetRenderDrawColor(renderer, 0xff, 0, 0, 0xff);
+        SDL_RenderDrawLine(renderer, x, 0, x, bound.h);
+
+        auto lineEnd = std::find(lineStart, u16str.end(), '\n');
+
+        std::u16string line(lineStart, lineEnd);
+
+        hb_buffer_t* buffer = hb_buffer_create();
+        hb_buffer_set_direction(buffer, HB_DIRECTION_TTB);
+        hb_buffer_set_script(buffer, script);
+
+        hb_buffer_add_utf16(buffer,
+            reinterpret_cast<const uint16_t*>(line.c_str()),
+            line.size(), 0, line.size());
+
+        hb_shape(font.HbFont(), buffer, NULL, 0);
+
+        unsigned int glyph_count = hb_buffer_get_length(buffer);
+        hb_glyph_info_t* glyph_infos = hb_buffer_get_glyph_infos(buffer, NULL);
+        hb_glyph_position_t* glyph_positions =
+            hb_buffer_get_glyph_positions(buffer, NULL);
+
+        int y = 0;
+
+        for (int i = 0; i < glyph_count; i++) {
+            auto index = glyph_infos[i].codepoint;
+
+            auto& g = font.GetGlyph(renderer, index);
+            if (g.texture != nullptr) {
+                SDL_SetTextureColorMod(g.texture, color.r, color.g, color.b);
+
+                SDL_Rect dst = g.bound;
+                dst.x += x + (glyph_positions[i].x_offset * font.Scale());
+                dst.y = y + dst.y - (glyph_positions[i].y_offset * font.Scale());
+
+                SDL_RenderCopy(renderer, g.texture, nullptr, &dst);
+            }
+            y += glyph_positions[i].y_advance;
+        }
+        hb_buffer_destroy(buffer);
+
+        if (lineEnd == u16str.end())
+            break;
+
+        lineStart = lineEnd + 1;
+        x -= -font.Descend() + font.LineGap() + font.Ascend();
+    }
+}
