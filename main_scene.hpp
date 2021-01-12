ef89a16d5d0a4eb64598d69f08e91c21e78bfdc5
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2021-01-13 02:52:49 +0700

    Add unimplemented RTL.

--- main_scene.hpp
+++ main_scene.hpp
@@ -41,17 +41,19 @@ private:
 
   Font font{};
 
-  static constexpr std::array<hb_script_t, 6> scripts = {
+  static constexpr std::array<hb_script_t, 7> scripts = {
       HB_SCRIPT_COMMON,   HB_SCRIPT_THAI, HB_SCRIPT_HIRAGANA,
-      HB_SCRIPT_KATAKANA, HB_SCRIPT_HAN,  HB_SCRIPT_HANGUL};
+      HB_SCRIPT_KATAKANA, HB_SCRIPT_HAN,  HB_SCRIPT_HANGUL,
+      HB_SCRIPT_ARABIC};
 
-  static constexpr std::array<char *, 6> scriptStrs = {
-      "Common", "Thai", "Hiragana", "Katakana", "Han", "Hangul"};
+  static constexpr std::array<char *, 7> scriptStrs = {
+      "Common", "Thai", "Hiragana", "Katakana", "Han", "Hangul", "Arabic"};
 
-  static constexpr std::array<hb_direction_t, 2> directions = { HB_DIRECTION_LTR, HB_DIRECTION_TTB };
-    
+  static constexpr std::array<hb_direction_t, 3> directions = {
+      HB_DIRECTION_LTR, HB_DIRECTION_RTL, HB_DIRECTION_TTB};
 
-  static constexpr std::array<char*, 2> directionStrs = {"Left to Right", "Top to Bottom"};
+  static constexpr std::array<char *, 3> directionStrs = {
+      "Left to Right", "Right To Left", "Top to Bottom"};
 
   int selectedScript = 0;
   int selectedDirection = 0;
--- text_renderer.hpp
+++ text_renderer.hpp
@@ -4,12 +4,7 @@
 #include <SDL2/SDL.h>
 #include <harfbuzz/hb.h>
 
-
-enum class TextRenderEnum {
-    NoShape,
-    LeftToRight,
-    TopToBottom
-};
+enum class TextRenderEnum { NoShape, LeftToRight, RightToLeft, TopToBottom };
 class Font;
 
 typedef std::function<void(SDL_Renderer *, const SDL_Rect &bound, Font &font,
@@ -22,6 +17,10 @@ void TextRenderNoShape(SDL_Renderer *, const SDL_Rect &bound, Font &font,
                        const hb_script_t &script);
 
 void TextRenderLeftToRight(SDL_Renderer *, const SDL_Rect &bound, Font &font,
+                           const std::string &str, const SDL_Color &,
+                           const hb_script_t &script);
+
+void TextRenderRightToLeft(SDL_Renderer *, const SDL_Rect &bound, Font &font,
                            const std::string &str, const SDL_Color &,
                            const hb_script_t &script);
 
