1ab67827c7b13297fdaed8e12ef6dd05351e73d4
Wutipong Wongsakuldej <wutipong@trueaxiongames.com>
2021-01-25 20:43:50 +0700

    fix the background color.

--- context.hpp
+++ context.hpp
@@ -3,6 +3,7 @@
 
 #include <SDL2/SDL.h>
 #include <filesystem>
+#include <glm/glm.hpp>
 
 constexpr int WIDTH = 800;
 constexpr int HEIGHT = 600;
@@ -16,7 +17,7 @@ struct Context {
 
   const SDL_Color debugGlyphBoundColor = {0xff, 0xff, 0x80, 0xff};
   const SDL_Color debugLineColor = {0x80, 0xff, 0x80, 0xff};
-  const SDL_Color backgroundColor = {0x50, 0x82, 0xaa, 0xff};
+  const glm::vec4 backgroundColor = { 0.313725501f, 0.509803951f, 0.666666687f, 1.0f };
 };
 
 void SaveContext(const Context& ctx, const std::filesystem::path& path);
