4c43421f99061ff0589153c877fef40f16a12c65
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2021-01-31 01:07:06 +0700

    Show an error when font file is invalid.

--- main_scene.cpp
+++ main_scene.cpp
@@ -13,7 +13,7 @@
 
 bool MainScene::Init(Context &context) {
 
-    Font::Init();
+  Font::Init();
   InitTextRenderers();
   dirChooser.SetTitle("Browse for font directory");
   OnDirectorySelected(context, context.fontPath);
@@ -52,10 +52,14 @@ void MainScene::Tick(Context &context) {
                   scripts[selectedScript]);
 }
 
-void MainScene::Cleanup(Context& context) { CleanUpTextRenderers(); Font::CleanUp(); }
+void MainScene::Cleanup(Context &context) {
+  CleanUpTextRenderers();
+  Font::CleanUp();
+}
 
 void MainScene::DoUI(Context &context) {
   int newSelected = selectedFontIndex;
+  
   ImGui::Begin("Menu");
   {
     if (ImGui::CollapsingHeader("Font Directory",
@@ -138,12 +142,28 @@ void MainScene::DoUI(Context &context) {
   }
 
   if (newSelected != selectedFontIndex) {
-    selectedFontIndex = newSelected;
-    if (selectedFontIndex == -1) {
+    if (newSelected == -1) {
       font = Font();
+      selectedFontIndex = newSelected;
     } else {
-      font.LoadFile(fontPaths[selectedFontIndex].string());
+        Font newFont;
+        if (!newFont.LoadFile(fontPaths[newSelected].string())) {
+            ImGui::OpenPopup("InvalidFont");
+        }
+        else {
+            font = newFont;
+            selectedFontIndex = newSelected;
+        }
     }
+  }
+
+  if (ImGui::BeginPopup("InvalidFont")) {
+      ImGui::Text("Invalid Font File");
+      if (ImGui::Button("Close"))
+      {
+          ImGui::CloseCurrentPopup();
+      }
+      ImGui::EndPopup();
   }
 }
 
