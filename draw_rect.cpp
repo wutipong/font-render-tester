82bc666d38025df0e4e5d9ab5b1f5d22d3ad034c
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2022-07-05 20:47:34 +0700

    add DrawRectMode

--- draw_rect.cpp
+++ draw_rect.cpp
@@ -12,11 +12,11 @@ static GLuint fragShader;
 static GLuint vao;
 static GLuint vbo;
 
-
-
 void InitDrawRect() {
-  vertShader = CompileShader("shaders/draw_rect.vert", "Draw Rect Vertex", GL_VERTEX_SHADER);
-  fragShader = CompileShader("shaders/draw_rect.frag", "Draw Rect Fragment", GL_FRAGMENT_SHADER);
+  vertShader = CompileShader("shaders/draw_rect.vert", "Draw Rect Vertex",
+                             GL_VERTEX_SHADER);
+  fragShader = CompileShader("shaders/draw_rect.frag", "Draw Rect Fragment",
+                             GL_FRAGMENT_SHADER);
 
   program = glCreateProgram();
   glAttachShader(program, vertShader);
@@ -28,7 +28,6 @@ void InitDrawRect() {
   glCreateVertexArrays(1, &vao);
   SetGLObjectLabel(GL_VERTEX_ARRAY, vao, "Draw Rect VAO");
   glBindVertexArray(vao);
-
 
   glGenBuffers(1, &vbo);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
@@ -58,7 +57,7 @@ void CleanUpDrawRect() {
 
 void DrawRect(const float &x, const float &y, const float &w, const float &h,
               const glm::vec4 &color, const int &screenWidth,
-              const int &screenHeight) {
+              const int &screenHeight, const DrawRectMode &mode) {
   glUseProgram(program);
 
   glm::mat4 transform =
@@ -68,5 +67,15 @@ void DrawRect(const float &x, const float &y, const float &w, const float &h,
   glUniform4f(1, color.r, color.g, color.b, color.a);
   glUniformMatrix4fv(2, 1, GL_FALSE, glm::value_ptr(transform));
 
-  glDrawArrays(GL_LINE_LOOP, 0, 4);
+  GLenum drawMode = GL_LINE_LOOP;
+  switch (mode) {
+  case DrawRectMode::Fill:
+    drawMode = GL_TRIANGLE_STRIP;
+    break;
+
+  case DrawRectMode::Outline:
+    drawMode = GL_LINE_LOOP;
+    break;
+  }
+  glDrawArrays(drawMode, 0, 4);
 }
--- draw_rect.hpp
+++ draw_rect.hpp
@@ -4,4 +4,11 @@
 void InitDrawRect();
 void CleanUpDrawRect();
 
-void DrawRect(const float& x, const float& y, const float& w, const float& h, const glm::vec4& color, const int& screenWidth, const int& screenHeight);
+enum class DrawRectMode
+{
+	Outline, Fill
+};
+
+void DrawRect(const float &x, const float &y, const float &w, const float &h,
+              const glm::vec4 &color, const int &screenWidth,
+              const int &screenHeight, const DrawRectMode& mode = DrawRectMode::Outline);
